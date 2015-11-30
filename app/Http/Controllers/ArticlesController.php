8935a936e8d7982cd6d78f2ef06d05a937a39eb0
Luke Lancaster <luketlancaster@gmail.com>
2015-11-30 13:28:56 -0600

    use built in validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,40 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 use Carbon\Carbon;
 use App\Article;
+use App\Http\Requests\CreateArticleRequest;
 
 class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::latest('published_at')->published()->get();
+
+        $articles = Article::orderBy('published_at')->published()->get();
 
         return view('articles.index', compact('articles'));
+
     }
 
     public function show($id)
     {
+
         $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+
     }
 
     public function create()
     {
+
         return view('articles.create');
+
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
-        Article::create(Request::all());
+        $this->validate($request, ['title' => 'required', 'body' => 'required']);
+        Article::create($request->all());
 
         return redirect('articles');
+
     }
+
+
 }
