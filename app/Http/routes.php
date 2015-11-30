c5c8f9a1b606e180bd2f22cb879319a0fac2e656
Luke Lancaster <luketlancaster@gmail.com>
2015-11-29 23:45:41 -0600

    move routes so wildcard catches last

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,5 +4,5 @@ Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
 Route::get('articles', 'ArticlesController@index');
-Route::get('articles/{id}', 'ArticlesController@show');
 Route::get('articles/create', 'ArticlesController@create');
+Route::get('articles/{id}', 'ArticlesController@show');
