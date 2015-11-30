ec9e76e0833f02a0dc05164649ec401addd4b1c0
Luke Lancaster <luketlancaster@gmail.com>
2015-11-30 14:49:04 -0600

    change request to be more modular

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
+
+
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use Carbon\Carbon;
 use App\Article;
-use App\Http\Requests\CreateArticleRequest;
+use App\Http\Requests\ArticleRequest;
 
 class ArticlesController extends Controller
 {
@@ -36,10 +38,27 @@ class ArticlesController extends Controller
 
     }
 
-    public function store(CreateArticleRequest $request)
+    public function store(ArticleRequest $request)
     {
 
         Article::create($request->all());
+
+        return redirect('articles');
+
+    }
+
+    public function edit($id)
+    {
+        $article = Article::findOrFail($id);
+
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update($id, ArticleRequest $request)
+    {
+        $article = Article::findOrFail($id);
+
+        $article->update($request->all());
 
         return redirect('articles');
 
