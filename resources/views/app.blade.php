048e2a8ef4de1f573d16af8d0c5fa530c1a95db2
Luke Lancaster <luketlancaster@gmail.com>
2015-11-29 22:56:40 -0600

    adding posts and tasks, initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @yield('footer')
+
+</body>
+</html>
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+
+  <h1>Contact Me!</h1>
+
+@stop
+
+@section('footer')
+
+    <script>alert('Load those scripts!');</script>
+
+@stop
